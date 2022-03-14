83385b24023a52eb8a84d0f2ce6da4c516fb9066
Davisralves <davialves324@gmail.com>
2022-03-14 10:38:45 -0300

    Testando header

--- front-end/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Header from './App';
-
-test('renders learn react link', () => {
-  render(<Header />);
-  const linkElement = screen.getByText('Como Jogar');
-  expect(linkElement).toBeInTheDocument();
-});
---
+++ front-end/src/helpers/renderWithRouter.js
@@ -0,0 +1,11 @@
+// ref https://testing-library.com/docs/example-react-router/#reducing-boilerplate
+import { BrowserRouter } from "react-router-dom"
+import { render } from "@testing-library/react"
+
+const renderWithRouter = (ui, {route = '/'} = {}) => {
+  window.history.pushState({}, 'Test page', route)
+
+  return render(ui, {wrapper: BrowserRouter})
+}
+
+export default renderWithRouter;
