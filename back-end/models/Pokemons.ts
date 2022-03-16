ee2a74cf820bfe5fb487aaba7b7e015919e0db60
Davisralves <davialves324@gmail.com>
2022-03-16 17:55:08 -0300

    adicionando imagem a resposta

--- back-end/models/Pokemons.ts
+++ back-end/models/Pokemons.ts
@@ -3,46 +3,65 @@ import {
 	Ipokemon,
 	IpokemonObject,
 	IpokemonDefaultObject,
-  Ifirst151Pokemons,
+	Ifirst151Pokemons,
 } from "../interfaces/Pokemons";
 
 const fetchFirst151Pokemons = async () => {
-  const pokemons = await fetch("https://pokeapi.co/api/v2/pokemon?limit=20");
-  return await pokemons.json();
-}
+	const pokemons = await fetch("https://pokeapi.co/api/v2/pokemon?limit=20");
+	return await pokemons.json();
+};
 
-const getPokemonsDetailsPromises = async (first151Pokemons: Ifirst151Pokemons) => {
-  return await first151Pokemons.results.map(
-    async ({ url }: Ipokemon) => {
-        const pokemonsDetails = await fetch(url);
-        return await pokemonsDetails.json();
-    });
-}
+const getPokemonsDetailsPromises = async (
+	first151Pokemons: Ifirst151Pokemons
+) => {
+	return await first151Pokemons.results.map(async ({ url }: Ipokemon) => {
+		const pokemonsDetails = await fetch(url);
+		return await pokemonsDetails.json();
+	});
+};
 
-const filterPokemonInfo = (pokemonsInfo: PromiseFulfilledResult<IpokemonDefaultObject>[]) => (
-  pokemonsInfo.map((pokemonDetail: PromiseFulfilledResult<IpokemonDefaultObject>) => {
-    const { name, types, height, weight } = pokemonDetail.value;
-    console.log(types[0].type.name);
-    const pokemonObject: IpokemonObject = {
-      name,
-      type1: types[0].type.name,
-      height,
-      weight,
-    };
-    if(types[1]) {
-      pokemonObject.type2 = types[1].type.name;
-    }
-    return pokemonObject;
-  }));
-
+const filterPokemonInfo = (
+	pokemonsInfo: PromiseFulfilledResult<IpokemonDefaultObject>[]
+) =>
+	pokemonsInfo.map(
+		(pokemonDetail: PromiseFulfilledResult<IpokemonDefaultObject>) => {
+			const {
+				name,
+				types,
+				height,
+				weight,
+				sprites: {
+					other: {
+						dream_world: { front_default: img },
+					},
+				},
+			} = pokemonDetail.value;
+			console.log(types[0].type.name);
+			const pokemonObject: IpokemonObject = {
+				name,
+				type1: types[0].type.name,
+				height,
+				weight,
+        img,
+			};
+			if (types[1]) {
+				pokemonObject.type2 = types[1].type.name;
+			}
+			return pokemonObject;
+		}
+	);
 
 const PokemonModel = {
 	fetchPokemons: async () => {
-    const first151Pokemons = await fetchFirst151Pokemons();
-    const pokemonDetailsPromises = await getPokemonsDetailsPromises(first151Pokemons);
-    const allPromises = await Promise.allSettled(pokemonDetailsPromises);
-    const resolvedPromises = allPromises.filter((resolvedPromise) => resolvedPromise.status === "fulfilled") as PromiseFulfilledResult<IpokemonDefaultObject>[];
-    const pokemonsWithDetails = filterPokemonInfo(resolvedPromises);
+		const first151Pokemons = await fetchFirst151Pokemons();
+		const pokemonDetailsPromises = await getPokemonsDetailsPromises(
+			first151Pokemons
+		);
+		const allPromises = await Promise.allSettled(pokemonDetailsPromises);
+		const resolvedPromises = allPromises.filter(
+			(resolvedPromise) => resolvedPromise.status === "fulfilled"
+		) as PromiseFulfilledResult<IpokemonDefaultObject>[];
+		const pokemonsWithDetails = filterPokemonInfo(resolvedPromises);
 		return pokemonsWithDetails;
 	},
 };
