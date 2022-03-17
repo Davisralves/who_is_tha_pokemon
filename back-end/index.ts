6cfd392d1cb97470dd251b3176aeb6c5fa77590b
Davisralves <davialves324@gmail.com>
2022-03-17 19:56:33 -0300

    alterando variaveis de ambiente

--- back-end/index.ts
+++ back-end/index.ts
@@ -9,8 +9,8 @@ app.use(cors());
 
 const { PORT } = process.env;
 
-app.get("/", requestPokemons);
+app.get("/pokemons", requestPokemons);
 
 app.listen(PORT, () => {
-	console.log(`Server is running at ${process.env.HOST}${PORT || 3000}`);
+	console.log(`Server is running at ${process.env.HOST}${PORT || 8000}`);
 });
