a0ec7a630a825ec9355240b14b35319dccfba9c1
Davisralves <davialves324@gmail.com>
2022-03-19 16:03:57 -0300

    finalizando testestes do model

--- tests/model.test.js
+++ tests/model.test.js
@@ -24,7 +24,7 @@ describe("Testa funcionamento das funções com o banco de dados", () => {
 	];
 
 	before(async () => {
-		sinon.stub(connection, "execute").resolves(pokemons);
+		sinon.stub(connection, "execute").resolves([pokemons]);
 	});
 
 	after(async () => {
@@ -32,12 +32,13 @@ describe("Testa funcionamento das funções com o banco de dados", () => {
 	});
 
 	describe('quando "getAll" é requisitado os pokemons são retornados corretamente para o services', () => {
-		it("retorna a uma objeto", async () => {
+		it("retorna a uma array", async () => {
 			const response = await PokemonsModel.getAll();
-			expect(response).to.be.a("object");
+			expect(response).to.be.a("array");
+      expect(response).to.have.lengthOf(2);
 		});
 		it("Retorna ao objeto pokemons", async () => {
-			const reponse = await PokemonsModel.getAll();
+			const [reponse] = await PokemonsModel.getAll();
 			expect(reponse).to.have.a.property('name');
 			expect(reponse).to.have.a.property('type1');
 			expect(reponse).to.have.a.property('type2');
@@ -46,4 +47,38 @@ describe("Testa funcionamento das funções com o banco de dados", () => {
 			expect(reponse).to.have.a.property('img');
 		});
 	});
+  
+  describe('quando "getByName" é requisitado o pokemon é retornado corretamente para o services', () => {
+		it("retorna a uma objeto", async () => {
+			const [response] = await PokemonsModel.getByName();
+			expect(response).to.be.a("object");
+		});
+		it("Retorna ao objeto pokemons", async () => {
+			const [reponse] = await PokemonsModel.getByName();
+			expect(reponse).to.have.a.property('name');
+			expect(reponse).to.have.a.property('type1');
+			expect(reponse).to.have.a.property('type2');
+			expect(reponse).to.have.a.property('weight');
+			expect(reponse).to.have.a.property('height');
+			expect(reponse).to.have.a.property('img');
+		});
+	});
+});
+
+describe('quando "registerNewPokemon" é requisitado corretamente a função retorna a um insertId', () => {
+
+  before(async () => {
+		sinon.stub(connection, "execute").resolves([{insertId: 1}]);
+	});
+
+	after(async () => {
+		connection.execute.restore();
+	});
+
+  it("InsertId é um número", async () => {
+    const response = await PokemonsModel.registerNewPokemon();
+    expect(response).to.be.a('number');
+    expect(response).to.be.equals(1);
+  });
+
 });
