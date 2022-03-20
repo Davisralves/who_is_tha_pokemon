01370c1cccedecd102755da73aff43d5d67c3e49
Davisralves <davialves324@gmail.com>
2022-03-20 17:55:51 -0300

    adicionando logica de selecionar pokemon na pokedex e introduzindo opção delete

--- front-end/src/components/Pokedex.tsx
+++ front-end/src/components/Pokedex.tsx
@@ -13,24 +13,79 @@ type pokemonLocation = {
 
 
 export default function Pokedex() {
-  const [loading, setLoading] = useState(true);
+	const [loading, setLoading] = useState(true);
+	const [input, setInput] = useState("");
+  const [message, setMessage] = useState('');
+  const [selectedPokemon, selectPokemon] = useState('');
+
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setInput(event.target.value);
+	};
+
+  const handleSelect = () => {
+    const filteredPokemons = pokemons.filter((pokemons) => pokemons.name.includes(input))
+    if(filteredPokemons.length === 1) {
+      setMessage('Selected !')
+      return selectPokemon(filteredPokemons[0].name)
+    } return setMessage('Have more than one pokemon in list. Please type the complete name')
+  }
+
+  const handleDelete = () => {
+    if(selectedPokemon === '') {
+      return setMessage('A pokemon must be Selected first')
+    } else {
+      setMessage('Deleting...');
+      
+    } 
+
+  }
 
 	const location = useLocation();
 	const {
 		state: { pokemons },
 	} = location as pokemonLocation;
-  if(pokemons.length > 2 && loading) {
-    setLoading(false);
-  }
+	if (pokemons.length > 2 && loading) {
+		setLoading(false);
+	}
 
 	return (
 		<main className="pokedex">
 			<h1 className="title">Pokedex</h1>
 			<PokedexHeader />
+			<br />
+			<label className="default-yellow-case" htmlFor="pokemonInput">
+				Search:
+			</label>
+			<input
+				className="submit-input"
+				type="text"
+				onChange={(e) => handleChange(e)}
+			/>
+			<button
+				onClick={handleSelect}
+			>
+				{" "}
+				Select
+			</button>
+			<button
+				onClick={handleDelete}
+			>
+				{" "}
+				Delete
+			</button>
+			<button
+				// onClick={(event) => handleSubmit(event)}
+			>
+				{" "}
+				Edit
+			</button>
+      <h5>{message}</h5>
 			<section className="pokedex-main-section">
 				{!loading ? (
 					pokemons.map((pokemon: IpokemonObject, index: number) => (
-						<PokemonCard pokemon={pokemon} key={index} />
+            pokemon.name.includes(input) ?
+						<PokemonCard pokemon={pokemon} key={index} selected={pokemon.name === selectedPokemon}/>
+            : ''
 					))
 				) : (
 					<h3>Loading...</h3>
