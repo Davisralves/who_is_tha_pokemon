e42b146cb640c59a6f2eeba1505420f2b81540c8
Davisralves <davialves324@gmail.com>
2022-03-21 08:00:47 -0300

    preparando front-end para deploy

--- back-end/controller/middlewares/helpers.ts
+++
@@ -1,7 +0,0 @@
-export const isString = (value: any) => typeof value === 'string'; 
-
-export const isNumber = (value: any) => typeof value === 'number';
-
-export const AllAreString = (value: string[]) => value.every((item) => isString(item)); 
-
-export const AllAreNumbers = (value: string[]) => value.every((item) => isNumber(item)); 
